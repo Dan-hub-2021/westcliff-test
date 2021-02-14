2d83a35a2305d0853a3ee2921c9226ba23b259e2
Dan-hub-2021 <78669598+Dan-hub-2021@users.noreply.github.com>
2021-02-13 20:38:56 -0800

    Delete Web 301 - Wk 5 directory

--- Web 301 - Wk 5/Week 1 Day 1/homework-module2 - Dan Sy/js/onchange.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("mySelect").value;
-    document.getElementById("demo").innerHTML = "Enter your " + x;
-    if (x.style.display === "none") {
-      x.style.display = "";
-    } else {
-      x.style.display = "left";
-    }
-  }
